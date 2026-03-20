62cc98242963081c6d3ea629db60e9be1a3f113c
aarvisaini <aarviisaini@gmail.com>
2026-03-20 04:13:33 +0000

    trimester 2 lesson 9 homework 2 questions done

--- lesson9/homework/main.py
+++ lesson9/homework/main.py
@@ -10,3 +10,88 @@ def draw_star(size):
         turtle.right(144)
 draw_star(100)
 turtle.done()   
+
+
+
+# Problem 3
+# Draw the word "SOVA" in block letters using turtle lines.
+# (It does not have to be perfect.
+import turtle
+
+
+t = turtle.Turtle()
+t.pensize(10)
+t.color("lavender")
+t.speed(9)
+t.penup()
+t.goto(-190 ,-5)
+t.pendown()
+
+def draw_block_s():
+    t.forward(60)
+    t.left(90)
+    t.forward(60)
+    t.left(90)
+    t.forward(60)
+    t.right(90)
+    t.forward(60)
+    t.right(90)
+    t.forward(60)
+draw_block_s()
+
+t.penup()
+t.goto(-110,-5)
+t.pendown()
+
+def draw_block_o(width, height):
+    
+    
+    for _ in range(2):
+        t.forward(width)
+        t.left(90)
+        t.forward(height)
+        t.left(90)
+
+
+
+draw_block_o(70, 100)
+
+t.penup()
+t.goto(-15, 98)
+t.pendown()
+
+
+t.right(70)    
+t.forward(110)
+
+
+t.left(140)    
+t.forward(110)
+
+
+t.penup()
+t.goto(-50, -50)
+t.pendown()
+
+
+
+
+t.penup()
+t.goto(50,-5)
+t.pendown()
+
+t.left(1)
+t.forward(120)
+
+
+t.right(145)
+t.forward(120)
+
+
+t.penup()
+t.backward(80) 
+t.setheading(182) 
+t.pendown()
+t.forward(22)
+
+turtle.done()
